32fb6c4c88c0a7259cd7b98c1897038ce90d0b9c
Thongchai <thongchai.masilela@gmail.com>
2024-06-06 15:34:31 -0700

    Updated header

--- chemistry/TsChemTrackingAction.hh
+++ chemistry/TsChemTrackingAction.hh
@@ -1,7 +1,7 @@
 //
 // ********************************************************************
 // *                                                                  *
-// * Copyright 2024 The OpenTOPAS Collaboration                       *
+// * Copyright 2024 The TOPAS Collaboration                           *
 // * Copyright 2022 The TOPAS Collaboration                           *
 // *                                                                  *
 // * Permission is hereby granted, free of charge, to any person      *
